c6f368643888b9cb307a4b7e81fafc4175bdd9bc
Paul Tomchik <pauljtomchik@gmail.com>
2023-04-17 10:04:33 -0400

    Added transcom_events_onto_road_network to dt-transcom_events Task.

--- src/data_types/transcom/dt-transcom_events/tasks/publish/index.ts
+++ src/data_types/transcom/dt-transcom_events/tasks/publish/index.ts
@@ -23,7 +23,7 @@ export type FinalEvent = {
   type: ":FINAL";
 };
 
-async function moveStagedTranscomEventsToPublished(staging_schema: string) {
+async function publishTranscomEvents(staging_schema: string) {
   const indent = "".repeat(10);
 
   const conflictActionsHolders = dbCols
@@ -49,7 +49,7 @@ async function moveStagedTranscomEventsToPublished(staging_schema: string) {
               DO UPDATE
                 SET -- SEE: https://stackoverflow.com/a/40689501/3970755
                   ${conflictActionsHolders}
-        ; 
+        ;
       `,
       staging_schema,
       ...conflictActionFillers
@@ -61,7 +61,7 @@ async function moveStagedTranscomEventsToPublished(staging_schema: string) {
   await dama_db.query(sql);
 }
 
-async function moveStagedEventsToAdminGeomsToPublished(staging_schema: string) {
+async function publishTranscomEventsToAdminGeoms(staging_schema: string) {
   const sql = dedent(
     pgFormat(
       `
@@ -99,7 +99,7 @@ async function moveStagedEventsToAdminGeomsToPublished(staging_schema: string) {
               ua_name,
               ua_code
             FROM %I.transcom_event_administative_geographies
-        ; 
+        ;
       `,
       staging_schema,
       staging_schema
@@ -111,9 +111,7 @@ async function moveStagedEventsToAdminGeomsToPublished(staging_schema: string) {
   await dama_db.query(sql);
 }
 
-async function moveStagedEventsToConflationMapToPublished(
-  staging_schema: string
-) {
+async function publishTranscomEventsToConflationMap(staging_schema: string) {
   const table_name = `transcom_events_onto_conflation_map_${conflation_version}`;
 
   const {
@@ -237,7 +235,7 @@ async function moveStagedEventsToConflationMapToPublished(
 
               snap_pt_geom
             FROM %I.%I
-        ; 
+        ;
       `,
       table_name, // DELETE FROM
       staging_schema, // USING
@@ -251,6 +249,113 @@ async function moveStagedEventsToConflationMapToPublished(
   logger.silly(publish_sql);
 
   await dama_db.query(publish_sql);
+}
+
+async function createTranscomEventsOntoRoadNetworkView() {
+  const events_onto_cmap_table_name = `transcom_events_onto_conflation_map_${conflation_version}`;
+
+  const get_years_sql = dedent(
+    pgFormat(
+      `
+        SELECT DISTINCT
+            year
+          FROM _transcom_admin.%I
+      `,
+      events_onto_cmap_table_name
+    )
+  );
+
+  const { rows } = await dama_db.query(get_years_sql);
+
+  const event_years = rows.map(({ year }) => year);
+
+  const view_sql_select_stmts: string[] = [];
+
+  for (let event_year of event_years) {
+    event_year = +event_year;
+
+    const sql = pgFormat(
+      `
+          SELECT
+              a.event_id,
+              a.year,
+
+              b.event_type,
+              e.general_category    AS nysdot_general_category,
+              e.sub_category        AS nysdot_sub_category,
+              e.detailed_category   AS nysdot_detailed_category,
+
+              GREATEST(
+                b.start_date_time,
+                %L::TIMESTAMP                           -- <event_year>-01-01
+              ) AS start_date_time,
+
+              LEAST(
+                b.close_date,
+                %L::TIMESTAMP - '1 second'::INTERVAL    -- <event_year + 1>-01-01
+              ) AS close_date,
+
+              a.conflation_way_id,
+              a.conflation_node_id,
+
+              CASE
+                WHEN a.osm_fwd = 0
+                  THEN -a.conflation_node_id
+                ELSE a.conflation_node_id
+              END AS signed_conflation_node_id,
+
+              c.dir,
+              a.n,
+              c.osm,
+              a.osm_fwd,
+              a.both_directions,
+              c.ris,
+              c.tmc,
+
+              a._modified_timestamp AS transcom_event_modified_timestamp,
+
+              b.point_geom    AS transcom_event_point_geom,
+              a.snap_pt_geom  AS transcom_event_snapped_geom,
+              c.wkb_geometry  AS conflation_map_way_geom,
+              d.wkb_geometry  AS conflation_map_node_geom
+
+            FROM _transcom_admin.%I AS a
+              INNER JOIN _transcom_admin.transcom_events_expanded_view AS b
+                USING (event_id)
+              INNER JOIN conflation.%I AS c
+                ON ( a.conflation_way_id = c.id )
+              INNER JOIN conflation.%I AS d
+                ON ( a.conflation_node_id = d.id )
+              LEFT OUTER JOIN transcom.nysdot_transcom_event_classifications AS e
+                ON ( LOWER(b.event_type) = LOWER(e.event_type) )
+
+            WHERE ( a.year = %s )
+        `,
+      `${event_year}-01-01`,
+      `${event_year + 1}-01-01`,
+      events_onto_cmap_table_name,
+      `conflation_map_${event_year}_${conflation_version}`,
+      `conflation_map_${event_year}_nodes_${conflation_version}`,
+      event_year
+    );
+
+    view_sql_select_stmts.push(sql);
+  }
+
+  const query_sql = view_sql_select_stmts.join(`
+          UNION ALL`);
+
+  const create_view_sql = dedent(
+    pgFormat(
+      `
+        CREATE OR REPLACE VIEW _transcom_admin.%I
+          AS ${query_sql}
+      `,
+      `transcom_events_onto_road_network_${conflation_version}`
+    )
+  );
+
+  await dama_db.query(create_view_sql);
 }
 
 // NOTE:  Should not be run as a Queued Subtask because will need to be
@@ -280,9 +385,10 @@ export default async function main(etl_work_dir: string) {
     .get();
 
   await dama_db.runInTransactionContext(async () => {
-    await moveStagedTranscomEventsToPublished(staging_schema);
-    await moveStagedEventsToAdminGeomsToPublished(staging_schema);
-    await moveStagedEventsToConflationMapToPublished(staging_schema);
+    await publishTranscomEvents(staging_schema);
+    await publishTranscomEventsToAdminGeoms(staging_schema);
+    await publishTranscomEventsToConflationMap(staging_schema);
+    await createTranscomEventsOntoRoadNetworkView();
   });
 
   final_event = { type: ":FINAL" };
