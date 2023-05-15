e12158032195b0f1a7c904d24b948d83a654387f
Paul Tomchik <pauljtomchik@gmail.com>
2023-05-15 18:22:16 -0400

    Fix in dt-npmrds_travel_times_imp compute_statistics because server ran out of memory.

--- src/data_types/npmrds/dt-npmrds_travel_times_imp/tasks/compute_statistics.ts
+++ src/data_types/npmrds/dt-npmrds_travel_times_imp/tasks/compute_statistics.ts
@@ -21,61 +21,34 @@ export default async function main(
   tmc_identification_imp_table_schema: string,
   tmc_identification_imp_table_name: string
 ): Promise<DoneData> {
+  // NOTE: Encountered the following error when getting to aggressive with the string_agg
+  //
+  //       ERROR:  out of memory
+  //       DETAIL: Cannot enlarge string buffer containing 1073741822 bytes by 1 more bytes.
+  //
   const sql = dedent(
     pgFormat(
       `
-        WITH cte_data_md5sum AS (
-
-          SELECT
-              MD5(
-                string_agg(
-                  MD5(
-                    json_build_array(
-                      tmc,
-                      date,
-                      epoch,
-                      travel_time_all_vehicles,
-                      travel_time_passenger_vehicles,
-                      travel_time_freight_trucks,
-                      data_density_all_vehicles,
-                      data_density_passenger_vehicles,
-                      data_density_freight_trucks
-                    )::TEXT
-                  ), '|' ORDER BY tmc)
-              ) AS all_columns_md5sum,
-
-              MD5(
-                string_agg( DISTINCT tmc, '|' ORDER BY tmc)
-              ) AS tmcs_md5sum
-
-            FROM %I.%I
-        ), cte_summary_by_frc AS (
-
-          SELECT
-              json_object_agg(
-                COALESCE(frc::TEXT, 'null'),
-                summary
-              ) AS summary_by_frc
-            FROM (
-              SELECT
-                  frc,
-                  jsonb_build_object(
-                    'total_tmcs',         COUNT( DISTINCT tmc ),
-                    'total_records',      COUNT(1)
-                  ) AS summary
-                FROM %I.%I
-                  INNER JOIN %I.%I
-                    USING (tmc)
-                GROUP BY frc
-            ) AS t
-        )
-          SELECT
-              *
-            FROM cte_summary_by_frc AS a
-              CROSS JOIN cte_data_md5sum AS b
+        SELECT
+            json_object_agg(
+              COALESCE(frc::TEXT, 'null'),
+              summary
+            ) AS summary_by_frc
+          FROM (
+            SELECT
+                frc,
+                jsonb_build_object(
+                  'total_tmcs',             COUNT( DISTINCT tmc ),
+                  'total_records',          COUNT(1),
+                  tmcs_md5sum,              MD5(string_agg( DISTINCT tmc, '|' ORDER BY TMC)),
+                  travel_time_all_sum,      SUM(travel_time_all_vehicles)::TEXT
+                ) AS summary
+              FROM %I.%I
+                INNER JOIN %I.%I
+                  USING (tmc)
+              GROUP BY frc
+          ) AS t
       `,
-      npmrds_travel_times_imp_table_schema,
-      npmrds_travel_times_imp_table_name,
       npmrds_travel_times_imp_table_schema,
       npmrds_travel_times_imp_table_name,
       tmc_identification_imp_table_schema,
