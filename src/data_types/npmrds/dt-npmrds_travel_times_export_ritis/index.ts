5ebcfc06d4f58a0807df8986cf9813e9e431e05c
Paul Tomchik <pauljtomchik@gmail.com>
2023-05-16 19:03:02 -0400

    subtask idempotentcy in dt-npmrds_travel_times_export_ritis

--- src/data_types/npmrds/dt-npmrds_travel_times_export_ritis/index.ts
+++ src/data_types/npmrds/dt-npmrds_travel_times_export_ritis/index.ts
@@ -1,6 +1,6 @@
 import { join } from "path";
 
-import dama_events from "data_manager/events";
+import dama_events, { EtlEvent, DamaEvent } from "data_manager/events";
 
 import {
   getEtlContextId,
@@ -10,7 +10,11 @@ import {
 import BaseTasksController from "data_manager/tasks/BaseTasksController";
 import { DamaTaskDescriptor } from "data_manager/tasks/domain";
 
-import { TaskQueue, NpmrdsExportTransformOutput } from "../domain";
+import {
+  TaskQueue,
+  NpmrdsExportRequest,
+  NpmrdsExportTransformOutput,
+} from "../domain";
 
 import {
   InitialEvent as DownloadInitialEvent,
@@ -30,6 +34,115 @@ export type FinalEvent = {
   payload: DoneData;
 };
 
+enum SubtaskEventType {
+  REQUEST_AND_DOWNLOAD_QUEUED = "REQUEST_AND_DOWNLOAD_QUEUED",
+  TRANSFORM_QUEUED = "TRANSFORM_QUEUED",
+}
+
+async function download(
+  npmrds_export_request: NpmrdsExportRequest,
+  events: DamaEvent[],
+  task_controller: BaseTasksController
+) {
+  let request_and_download_queued_event: EtlEvent | undefined = events.find(
+    ({ type }) => type === SubtaskEventType.REQUEST_AND_DOWNLOAD_QUEUED
+  );
+
+  if (!request_and_download_queued_event) {
+    const request_and_download_initial_event = {
+      type: ":INITIAL",
+      payload: npmrds_export_request,
+      meta: { note: "request and download export" },
+    };
+
+    const download_task_desc: DamaTaskDescriptor = {
+      worker_path: download_worker_path,
+      dama_task_queue_name: TaskQueue.DOWNLOAD_EXPORT,
+      parent_context_id: getEtlContextId(),
+      initial_event: request_and_download_initial_event,
+      etl_work_dir: getEtlWorkDir(),
+    };
+
+    const { etl_context_id: download_eci } =
+      await task_controller.queueDamaTask(download_task_desc, {
+        retryLimit: 0, // Because we don't want to make duplicate requests. TODO: implement a way to check.
+        expireInHours: 24 * 7, // Because it may be deep in a queue.
+      });
+
+    request_and_download_queued_event = {
+      type: SubtaskEventType.REQUEST_AND_DOWNLOAD_QUEUED,
+      payload: {
+        etl_context_id: download_eci,
+      },
+    };
+
+    await dama_events.dispatch(request_and_download_queued_event);
+  }
+
+  const {
+    payload: { etl_context_id: download_eci },
+  } = <DamaEvent>request_and_download_queued_event;
+
+  const download_final_event = <DownloadFinalEvent>(
+    await dama_events.getEventualEtlContextFinalEvent(download_eci)
+  );
+
+  return download_final_event.payload;
+}
+
+async function transform(
+  download_done_data: DownloadFinalEvent["payload"],
+  events: DamaEvent[],
+  task_controller: BaseTasksController
+) {
+  let transform_queued_event: EtlEvent | undefined = events.find(
+    ({ type }) => type === SubtaskEventType.TRANSFORM_QUEUED
+  );
+
+  if (!transform_queued_event) {
+    const transform_initial_event = {
+      type: ":INITIAL",
+      payload: download_done_data,
+      meta: { note: "transform download" },
+    };
+
+    const transform_task_desc: DamaTaskDescriptor = {
+      worker_path: transform_worker_path,
+      dama_task_queue_name: TaskQueue.TRANSFORM_EXPORT,
+      parent_context_id: getEtlContextId(),
+      initial_event: transform_initial_event,
+      etl_work_dir: getEtlWorkDir(),
+    };
+
+    const { etl_context_id } = await task_controller.queueDamaTask(
+      transform_task_desc,
+      {
+        retryLimit: 0,
+        expireInHours: 24 * 7,
+      }
+    );
+
+    transform_queued_event = {
+      type: SubtaskEventType.TRANSFORM_QUEUED,
+      payload: {
+        etl_context_id,
+      },
+    };
+
+    await dama_events.dispatch(transform_queued_event);
+  }
+
+  const {
+    payload: { etl_context_id },
+  } = <DamaEvent>transform_queued_event;
+
+  const transform_final_event = <TransformFinalEvent>(
+    await dama_events.getEventualEtlContextFinalEvent(etl_context_id)
+  );
+
+  return transform_final_event.payload;
+}
+
 export default async function main(
   initial_event: InitialEvent
 ): Promise<DoneData> {
@@ -40,64 +153,31 @@ export default async function main(
   let final_event = events.find(({ type }) => type === ":FINAL");
 
   if (final_event) {
-    return final_event;
+    return final_event.payload;
   }
 
   const task_controller = new BaseTasksController();
 
-  const request_and_download_initial_event = {
-    ...initial_event,
-    meta: { note: "request and download export" },
-  };
+  const { payload: npmrds_export_request } = initial_event;
 
-  const download_task_desc: DamaTaskDescriptor = {
-    worker_path: download_worker_path,
-    dama_task_queue_name: TaskQueue.DOWNLOAD_EXPORT,
-    parent_context_id: getEtlContextId(),
-    initial_event: request_and_download_initial_event,
-    etl_work_dir: getEtlWorkDir(),
-  };
-
-  const { etl_context_id: download_eci } = await task_controller.queueDamaTask(
-    download_task_desc,
-    { retryLimit: 0, expireInHours: 24 * 7 }
+  const download_done_data = await download(
+    npmrds_export_request,
+    events,
+    task_controller
   );
 
-  const download_final_event = <DownloadFinalEvent>(
-    await dama_events.getEventualEtlContextFinalEvent(download_eci)
+  const transform_done_data = await transform(
+    download_done_data,
+    events,
+    task_controller
   );
-
-  const transform_initial_event = {
-    type: ":INITIAL",
-    payload: download_final_event.payload,
-    meta: { note: "transform download" },
-  };
-
-  const transform_task_desc: DamaTaskDescriptor = {
-    worker_path: transform_worker_path,
-    dama_task_queue_name: TaskQueue.TRANSFORM_EXPORT,
-    parent_context_id: getEtlContextId(),
-    initial_event: transform_initial_event,
-    etl_work_dir: getEtlWorkDir(),
-  };
-
-  const { etl_context_id: transform_eci } = await task_controller.queueDamaTask(
-    transform_task_desc,
-    { retryLimit: 0, expireInHours: 24 * 7 }
-  );
-
-  const transform_final_event = <TransformFinalEvent>(
-    await dama_events.getEventualEtlContextFinalEvent(transform_eci)
-  );
-
-  const { payload: done_data } = transform_final_event;
 
   final_event = {
     type: ":FINAL",
-    payload: done_data,
+    payload: transform_done_data,
   };
 
   await dama_events.dispatch(final_event);
 
-  return done_data;
+  return transform_done_data;
 }
