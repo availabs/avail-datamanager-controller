7f188d58fcb8be59af86b9d9f5fbf88673fba134
Paul Tomchik <pauljtomchik@gmail.com>
2024-02-08 12:17:01 -0500

    Had to update hard-coded date in TRANSCOM ETL to include 2024.

--- src/data_types/transcom/dt-transcom_events/constants/conflation_map_meta.ts
+++ src/data_types/transcom/dt-transcom_events/constants/conflation_map_meta.ts
@@ -1,6 +1,16 @@
 // FIXME: These need to eventually be accessible from the data_manager.views table.
+// FIXME: Until DaMa integration, could use
+//
+// npmrds_production=# \d conflation_map_versions_meta
+//       View "conflation.conflation_map_versions_meta"
+//     Column     |  Type   | Collation | Nullable | Default
+// ---------------+---------+-----------+----------+---------
+//  year          | integer |           |          |
+//  major_version | integer |           |          |
+//  minor_version | integer |           |          |
+//  patch_version | integer |           |          |
 
 export const conflation_version = "v0_6_0";
 
 export const min_year = 2016;
-export const max_year = 2023;
+export const max_year = 2024;
